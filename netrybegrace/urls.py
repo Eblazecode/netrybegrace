bd15e9123693bc26f205e46a61296064d6cb5c59
eblazecode <dynablaze@gmail.com>
2022-07-28 19:15:08 -1100

    modified urls path

--- netrybegrace/urls.py
+++ netrybegrace/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('netrybeapp/', include('netrybeapp.urls'))
+     path('netrybecloud/', include('netrybecloud.urls'))
 ]
